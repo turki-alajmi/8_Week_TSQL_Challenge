f336e46d38a22bb810ae097514d919949a854124
Turki <turki23-23@hotmail.com>
2026-03-01 17:50:55 +0300

    Solved Q5-10, added comments

--- 8_Week_Challenge/Case_Study_2_Pizza_Runner/A. Pizza Metrics/Pizza_metrics.sql
+++ 8_Week_Challenge/Case_Study_2_Pizza_Runner/A. Pizza Metrics/Pizza_metrics.sql
@@ -1,29 +1,128 @@
---Q1
+------------------------------------------------------------------------
+--Q01 How many pizzas were ordered?
+------------------------------------------------------------------------
 SELECT
     COUNT(order_id) AS order_count
 FROM customer_orders_clean;
 
---Q2
+------------------------------------------------------------------------
+--Q02 How many unique customer orders were made?
+------------------------------------------------------------------------
 SELECT
-    COUNT(DISTINCT order_id) AS Distinct_order_count
+    COUNT(DISTINCT order_id) AS distinct_order_count
 FROM customer_orders_clean;
 
---Q3
+------------------------------------------------------------------------
+--Q03 How many successful orders were delivered by each runner?
+------------------------------------------------------------------------
 SELECT
     runner_id,
     COUNT(pickup_time) AS delivery_counts
 FROM runner_orders_clean
 WHERE cancellation IS NULL
-GROUP BY runner_id;
+GROUP BY
+    runner_id;
 
---Q4
+------------------------------------------------------------------------
+--Q04 How many of each type of pizza was delivered?
+------------------------------------------------------------------------
 SELECT
     c.pizza_id,
-    count(*) AS counts
+    COUNT(*) AS counts
 FROM customer_orders_clean AS c
 INNER JOIN runner_orders_clean AS r
-    on r.order_id = c.order_id
+    ON r.order_id = c.order_id
 WHERE r.cancellation IS NULL
-GROUP BY c.pizza_id;
+GROUP BY
+    c.pizza_id;
 
---Q5
+------------------------------------------------------------------------
+--Q05 How many Vegetarian and Meatlovers were ordered by each customer?
+------------------------------------------------------------------------
+SELECT
+    c.customer_id,
+    p.pizza_name,
+    COUNT(p.pizza_name) AS pizza_counts
+FROM customer_orders_clean AS c
+INNER JOIN pizza_names AS p
+    ON c.pizza_id = p.pizza_id
+GROUP BY
+    c.customer_id,
+    p.pizza_name
+ORDER BY
+    c.customer_id ASC,
+    p.pizza_name;
+
+------------------------------------------------------------------------
+--Q06 What was the maximum number of pizzas delivered in a single order?
+------------------------------------------------------------------------
+WITH pizza_counts AS (
+    SELECT
+        c.order_id,
+        COUNT(c.order_id) AS counts
+    FROM customer_orders_clean AS c
+    INNER JOIN runner_orders_clean AS r
+        ON c.order_id = r.order_id
+    WHERE r.cancellation IS NULL
+    GROUP BY c.order_id
+)
+SELECT
+    MAX(counts) AS max_pizza
+FROM pizza_counts;
+
+------------------------------------------------------------------------
+--Q07 For each customer, how many delivered pizzas had at least 1
+--              change and how many had no changes?
+------------------------------------------------------------------------
+SELECT
+    c.customer_id,
+    SUM(CASE
+            WHEN c.exclusions IS NULL AND c.extras IS NULL
+                THEN 1
+            ELSE 0 END) AS no_change,
+    SUM(CASE
+            WHEN c.exclusions IS NOT NULL OR c.extras IS NOT NULL
+                THEN 1
+            ELSE 0 END) AS with_change
+FROM customer_orders_clean AS c
+INNER JOIN runner_orders_clean AS r
+    ON c.order_id = r.order_id
+WHERE r.cancellation IS NULL
+GROUP BY
+    c.customer_id;
+
+------------------------------------------------------------------------
+--Q08 How many pizzas were delivered that had both exclusions and extras?
+------------------------------------------------------------------------
+SELECT
+    COUNT(*) AS order_counts
+FROM customer_orders_clean AS c
+INNER JOIN runner_orders_clean AS r
+    ON r.order_id = c.order_id
+WHERE r.cancellation IS NULL
+  AND (c.extras IS NOT NULL AND c.exclusions IS NOT NULL);
+
+------------------------------------------------------------------------
+--Q09 What was the total volume of pizzas ordered for each hour of the day?
+------------------------------------------------------------------------
+SELECT
+    DATEPART(HOUR, order_time) AS hours_histo,
+    COUNT(*) AS counts_pizza
+FROM customer_orders_clean
+GROUP BY
+    DATEPART(HOUR, order_time)
+ORDER BY
+    hours_histo ASC;
+
+------------------------------------------------------------------------
+--Q10 What was the volume of orders for each day of the week?
+------------------------------------------------------------------------
+SELECT
+    DATENAME(WEEKDAY, order_time) AS week_day,
+    COUNT(*) AS counts_pizza
+FROM customer_orders_clean
+GROUP BY
+    DATENAME(WEEKDAY, order_time),
+    DATEPART(WEEKDAY, order_time)
+ORDER BY
+    DATEPART(WEEKDAY, order_time) ASC;
--- 8_Week_Challenge/Case_Study_2_Pizza_Runner/DDL/DataClean_DDL.sql
+++ 8_Week_Challenge/Case_Study_2_Pizza_Runner/DDL/DataClean_DDL.sql
@@ -6,22 +6,22 @@
 -- 3. Materializes the clean data into a permanent table for analysis.
 -- ==========================================================================================
 SELECT
-        order_id,
-        customer_id,
-        pizza_id,
-        CAST(CASE
-                 WHEN TRIM(LOWER(exclusions)) IN ('null', '', 'nan')
-                     THEN NULL
-                 ELSE REPLACE(exclusions, ' ', '')
-            END
+    order_id,
+    customer_id,
+    pizza_id,
+    CAST(CASE
+             WHEN TRIM(LOWER(exclusions)) IN ('null', '', 'nan')
+                 THEN NULL
+             ELSE REPLACE(exclusions, ' ', '')
+        END
         AS VARCHAR(50)) AS exclusions,
-        CAST(CASE
-            WHEN TRIM(LOWER(extras)) IN ('null','','nan')
-                THEN NULL
-            ELSE REPLACE(extras,' ','')
+    CAST(CASE
+             WHEN TRIM(LOWER(extras)) IN ('null', '', 'nan')
+                 THEN NULL
+             ELSE REPLACE(extras, ' ', '')
         END
         AS VARCHAR(50)) AS extras,
-        order_time
+    order_time
 INTO customer_orders_clean
 FROM customer_orders;
 
@@ -34,48 +34,52 @@ FROM customer_orders;
 -- 4. Materializes the clean data into a permanent table for analysis.
 -- ==========================================================================================
 WITH nulling AS (
-   SELECT
-    order_id,
-    runner_id,
-    CASE WHEN TRIM(LOWER(pickup_time)) IN ('null','nan','')
-        THEN NULL
-        ELSE pickup_time
-    END AS pickup_time,
-    CASE WHEN TRIM(LOWER(distance)) IN ('null','nan','')
-        THEN NULL
-        ELSE distance
-    END AS distance,
-    CASE WHEN TRIM(LOWER(duration)) IN ('null','nan','')
-        THEN NULL
-        ELSE duration
-    END AS duration,
-    CASE WHEN TRIM(LOWER(cancellation)) IN ('null','nan','')
-        THEN NULL
-        ELSE cancellation
-    END AS cancellation
-FROM runner_orders
-),
-    string_fix AS (
     SELECT
         order_id,
         runner_id,
-        pickup_time,
-        TRIM(REPLACE(LOWER(distance),'km','')) AS distance,
         CASE
-            WHEN PATINDEX('%m%',duration) > 0
-                THEN TRIM(LEFT(duration,(PATINDEX('%m%',duration))-1))
-            ELSE TRIM(duration)
-        END  AS duration,
-        --Dynamically extracts the number by slicing everything to the left of 'm' (handles both 'mins' and 'minutes' etc.)
-        cancellation
+            WHEN TRIM(LOWER(pickup_time)) IN ('null', 'nan', '')
+                THEN NULL
+            ELSE pickup_time
+            END AS pickup_time,
+        CASE
+            WHEN TRIM(LOWER(distance)) IN ('null', 'nan', '')
+                THEN NULL
+            ELSE distance
+            END AS distance,
+        CASE
+            WHEN TRIM(LOWER(duration)) IN ('null', 'nan', '')
+                THEN NULL
+            ELSE duration
+            END AS duration,
+        CASE
+            WHEN TRIM(LOWER(cancellation)) IN ('null', 'nan', '')
+                THEN NULL
+            ELSE cancellation
+            END AS cancellation
+    FROM runner_orders
+),
+    string_fix AS (
+        SELECT
+            order_id,
+            runner_id,
+            pickup_time,
+            TRIM(REPLACE(LOWER(distance), 'km', '')) AS distance,
+            CASE
+                WHEN PATINDEX('%m%', duration) > 0
+                    THEN TRIM(LEFT(duration, (PATINDEX('%m%', duration)) - 1))
+                ELSE TRIM(duration)
+                END AS duration,
+            --Dynamically extracts the number by slicing everything to the left of 'm' (handles both 'mins' and 'minutes' etc.)
+            cancellation
 
-    FROM nulling
+        FROM nulling
     )
 SELECT
     order_id,
     runner_id,
     CAST(pickup_time AS DATETIME) AS pickup_time,
-    CAST(distance AS decimal(5,1)) AS distance,
+    CAST(distance AS DECIMAL(5, 1)) AS distance,
     CAST(duration AS INTEGER) AS duration,
     cancellation
 INTO runner_orders_clean
